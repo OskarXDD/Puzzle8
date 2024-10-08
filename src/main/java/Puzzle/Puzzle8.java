92d0babd00a40df12dbb77fa2eeeba51f0a32c71
Oscar <Oscar@PC>
2024-10-08 16:05:39 -0600

    Carga Inicial

--- src/main/java/Puzzle/Puzzle8.java
+++ src/main/java/Puzzle/Puzzle8.java
@@ -15,8 +15,8 @@ public class Puzzle8 {
      public boolean esEstadoFinal() {
         int[][] estadoFinal = {
             {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 0}
+            {8, 0, 4},
+            {7, 6, 5}
         };
         return movimiento.esEstadoFinal(estadoFinal);
     }
